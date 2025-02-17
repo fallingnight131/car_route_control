ef3c61b4d9acd839b1e3ef390cf51f5826c2f32d
Fallingnight <fallingnight131@outlook.com>
2025-02-18 00:45:57 +0800

    修改auto_2赛道信息

--- script/set_track_auto_2.py
+++ script/set_track_auto_2.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.util.track_file_util import save_track_data
 
-track_outer = [(150, 150), (650, 150), (750, 200), (700, 300), (600, 400), (500, 450), 
+track_outer = [(150, 150), (650, 150), (700, 250), (700, 300), (600, 400), (500, 450), 
                (400, 500), (300, 450), (200, 400), (100, 300), (100, 200)]
 track_inner = [(200, 200), (600, 200), (650, 250), (600, 350), (500, 400), (400, 450), 
                (300, 400), (200, 350), (150, 250), (200, 200)]
